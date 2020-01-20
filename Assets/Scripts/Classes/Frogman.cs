5bb6d928c7768eb85dd5b2f6444af5756736cc88
Leo Sidebo <abletoirper@gmail.com>
2020-01-20 22:47:45 +0100

    Changed the sprite, fixed animation and added turn

--- Assets/Scripts/Classes/Frogman.cs
+++ Assets/Scripts/Classes/Frogman.cs
@@ -7,6 +7,7 @@ public class Frogman : MonoBehaviour
     private float jumpForce;
     private float moveSpeed;
     private bool enraged;
+    private bool facingRight;
     private Rigidbody2D rigidbody;
 
     public Frogman(Rigidbody2D rb)
@@ -15,12 +16,39 @@ public class Frogman : MonoBehaviour
         enraged = false;
         moveSpeed = 5f;
         jumpForce = 7f;
+        facingRight = false;
+    }
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        enraged = false;
+        moveSpeed = 5f;
+        jumpForce = 7f;
+        facingRight = false;
     }
 
     public void changeVelocity()
     {
-        Debug.Log("yee");
         rigidbody.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rigidbody.velocity.y);
+    }
+
+    public void animateMovement(Animator animator)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(Input.GetAxis("Horizontal") * moveSpeed));
+    }
+
+    public void flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public bool getFacingRight()
+    {
+        return facingRight;
     }
 
 }
--- Assets/Scripts/Controllers/FrogmanController.cs
+++ Assets/Scripts/Controllers/FrogmanController.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public class FrogmanController : MonoBehaviour
 {
-    private Frogman frogman;
+    public Frogman frogman;
+    public Animator animator;
+    private float horizontal;
+
     void Start()
     {
-        frogman = new Frogman(GetComponent<Rigidbody2D>());   
+        // frogman = new Frogman(GetComponent<Rigidbody2D>());   
     }
 
     void FixedUpdate()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        if (horizontal > 0 && !frogman.getFacingRight() || horizontal < 0 && frogman.getFacingRight())
+        {
+            frogman.flip();
+        }
+
         frogman.changeVelocity();
+        frogman.animateMovement(animator);
     }
 }
